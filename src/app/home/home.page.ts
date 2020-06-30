1e784551bc31a5741e5d87619e956551e58e9bff
Thanasis <5862461+saxa2393@users.noreply.github.com>
2020-06-30 16:54:54 +0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,19 @@ export class HomePage {
 
   constructor() {}
 
+  entry : string;
+  description : string;
+  objectForDB : Object;
+  allTypes = ["UNSET_DATA_TYPE", "BYTES", "BOOLEAN", "NUMBER", "STRING", "ENUM","STRUCT","LAT_LONG"];
+  dataType : string;
+  numberOfFields = [];
+  numberOfCards  = 0;
+
+  createCards(cards : number){
+    for(let i=1;i<=cards;i++){
+      this.numberOfFields.push(i);
+    }
+    this.numberOfCards  = 0;
+    this.dataType = '';
+  }
 }
---
+++ src/app/information-card/information-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-card',
+  templateUrl: './information-card.component.html',
+  styleUrls: ['./information-card.component.scss'],
+})
+export class InformationCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
