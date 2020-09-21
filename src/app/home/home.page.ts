d6f078f4d26a8ec8d96fa2bbe9c9af55587ffcd4
Thanasis <5862461+saxa2393@users.noreply.github.com>
2020-09-21 15:58:30 +0300

    working version
    confirm db Button should be clicked only once. Should find way to export json

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { InformationCardComponent } from './../information-card/information-card.component';
 import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { InfoService } from './../services/info.service';
+// import { File } from '@ionic-native/file/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // , public file: File
   constructor(private infoExchange: InfoService) { }
   schemaName = '';
   description = '';
@@ -47,36 +50,111 @@ export class HomePage {
       const titleLength = (element.title).length;
       if (titleLength === 1) {
         this.array1.push(element);
-        console.log(this.array1);
-
       }
       else if (titleLength === 2) {
         this.array2.push(element);
-        console.log(this.array2);
-
       }
       else if (titleLength === 3) {
         this.array3.push(element);
-        console.log(this.array3);
-
       }
       else if (titleLength === 4) {
         this.array4.push(element);
+      }
+      else if (titleLength === 5) {
+        this.array5.push(element);
+      }
+      else if (titleLength === 6) {
+        this.array6.push(element);
       }
     });
     this.confirm();
 
   }
-  confirm(){
-    this.array1.forEach(element => {
-      this.array2.forEach(element2 => {
-        if (element.title[0] === element2.title[0]){
-          element.structProperties.push(element2);
+  uri;
+  confirm() {
+    // the number of the array (ex: 5 , 6) indicates the depth in 
+    this.array5.forEach(element => {
+      this.array6.forEach(element2 => {
+        if (element.title[0] === element2.title[0] && element.title[1] === element2.title[1]
+          && element.title[2] === element2.title[2] && element.title[3] === element2.title[3] && element.title[4] === element2.title[4]) {
+          // element.structProperties.push(element2);
+          const { title, ...rest } = element2;
+
+          element.structProperties.push(rest);
         }
 
       });
     });
-    console.log(this.array1);
+    this.array4.forEach(element => {
+      this.array5.forEach(element2 => {
+        if (element.title[0] === element2.title[0] && element.title[1] === element2.title[1]
+          && element.title[2] === element2.title[2] && element.title[3] === element2.title[3]) {
+          // element.structProperties.push(element2);
+          const { title, ...rest } = element2;
+
+          element.structProperties.push(rest);
+        }
+
+      });
+    });
+    this.array3.forEach(element => {
+      this.array4.forEach(element2 => {
+        if (element.title[0] === element2.title[0] && element.title[1] === element2.title[1] && element.title[2] === element2.title[2]) {
+          // element.structProperties.push(element2);
+          const { title, ...rest } = element2;
+
+          element.structProperties.push(rest);
+        }
+
+      });
+    });
+    this.array2.forEach(element => {
+      this.array3.forEach(element2 => {
+        if (element.title[0] === element2.title[0] && element.title[1] === element2.title[1]) {
+          // element.structProperties.push(element2);
+          const { title, ...rest } = element2;
+
+          element.structProperties.push(rest);
+        }
+
+      });
+    });
+    this.array1.forEach(element => {
+      this.array2.forEach(element2 => {
+        if (element.title[0] === element2.title[0]) {
+          // tslint:disable-next-line: no-shadowed-variable
+          const { title, ...rest } = element2;
+
+          element.structProperties.push(rest);
+        }
+
+      });
+
+    });
+    // console.log(this.array1);
+
+
+    // #2 Converting the object to JSON...
+
+    // const json = JSON.stringify(this.array1);
+
+    // console.log(json);
+    this.array1.forEach(element => {
+      delete element.title;
+
+    });
+    const efactorySchema = {
+      action: 1,
+      schemaCreate: {
+        schemaName: this.schemaName,
+        description: this.description,
+        properties: this.array1
+      }
+    };
+    const json1 = JSON.stringify(efactorySchema);
+    // this.uri = "data:application/json;charset=UTF-8," + encodeURIComponent(json);
+
+    console.log(json1);
   }
 
 }
